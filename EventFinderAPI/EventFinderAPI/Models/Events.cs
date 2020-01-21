5102a6c0ee6a9a7e7c1f602b26208540b988ebd3
kee cha <kee_cha18@yahoo.com>
2020-01-21 14:34:47 -0600

    add webAPI file to application

---
+++ EventFinderAPI/EventFinderAPI/Models/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EventFinderAPI.Models
+{
+    public class Events
+    {
+        [Key]
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string VenueName { get; set; }
+        public string Date { get; set; }
+        public string Category { get; set; }
+        public string SubCategory { get; set; }
+    }
+}
